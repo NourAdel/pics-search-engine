11d3c65aeed374f0254a181bf774b46463ae7c9c
Nour Adel <noura7305@gmail.com>
2019-11-04 17:01:37 +0200

    on change event handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class SearchBar
- extends Component {
-    state = {  }
-    render() { 
-        return <div>Search</div>;
+class SearchBar extends Component {
+    onInputChange(event){
+        console.log(event.target.value);
+    }    
+    
+    render() {
+        return (
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>image search</label>
+                        <input type='text' onChange={this.onInputChange}></input>
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
- 
-export default SearchBar
-;
+
+export default SearchBar;
