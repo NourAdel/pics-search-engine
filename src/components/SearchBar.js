e8afab0f2d9bc0a84c786afc2233d66f41f95745
Nour Adel <noura7305@gmail.com>
2019-11-05 09:59:43 +0200

    children callbacks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,31 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
-  state= {term: ''}  
-    
-    render() {
-        return (
-            <div className="ui segment">
-                <form className="ui form">
-                    <div className="field">
-                        <label>image search</label>
-                        <input type='text'
-                        value={this.state.term}
-                         onChange={event =>this.setState({term: event.target.value})}></input>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+  state = { term: "" };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>image search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={event => this.setState({ term: event.target.value })}
+            ></input>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
+// uncontrolled: values stored in the HTML documents, pulled by DOM
+// controlled: values stored in the react side
