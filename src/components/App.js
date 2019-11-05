4a78177af4bab2b2fdea8aabeafc46b26c4b8ec8
Nour Adel <noura7305@gmail.com>
2019-11-05 10:28:30 +0200

    Api configration and 'async'

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
+  async onSearchSubmit(term) {
     console.log(term);
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 9232eb4f238f09c5555e7ea8f4faf835c442e1eefd33e3e5fb6f622593b3422e"
+      }
+    });
+    // same as 'async'
+    //.then((response)=> {
+    console.log(response.data.results);
   }
+
 
   render() {
     return (
